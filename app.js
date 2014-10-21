f80f209d091cefc101635e761b8bc4722291a522
soomtong <soomtong@gmail.com>
2014-10-21 15:41:49 +0900

    `update` index route

--- app.js
+++ app.js
@@ -129,7 +129,11 @@ app.get('/', function (req, res) {
     var params = {};
 
     req.session.clientRoute = null;
-    res.render('index', params);
+    if (req.isAuthenticated()) {
+        res.redirect('/dashboard');
+    } else {
+        res.render('index', params);
+    }
 });
 app.get('/login', accountController.loginForm);
 app.post('/login', accountController.login);
