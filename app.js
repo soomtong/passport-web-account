ca1d8501055bb63801124b4ff032dc5c49cd1f4c
soomtong <soomtong@gmail.com>
2014-11-01 18:37:30 +0900

    `update` password reset, more details

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ app.use(function(req, res, callback) {
     res.locals.site = {
         title: "Haroo Cloud Service Hub",
         url: app.get('hostEnv') == 'production' ? common['clientAuthUrl'] : '//localhost:' + common['port'],
-        dbHost: database['couch']['host']
+        dbHost: database['couch']['host'],
+        mailHost: common['mailServer']
     };
     callback();
 });
